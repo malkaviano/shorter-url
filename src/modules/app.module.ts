a03ec4b5c46feae5b7e7808305327f056e0eb6db
Rafael Karayannopoulos <rafael.kss@gmail.com>
2020-08-03 22:35:01 -0300

    add winston logger (#9)

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -6,15 +6,26 @@ import { AppService } from '@services/app.service';
 import { UserModule } from '@modules/user.module';
 import { UrlModule } from '@modules/url.module';
 import { StatsModule } from '@modules/stats.module';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from '../configs/winston.config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggerInterceptor } from '../interceptors/logger.interceptor';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
+    WinstonModule.forRoot(winstonConfig),
     UserModule,
     UrlModule,
     StatsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
